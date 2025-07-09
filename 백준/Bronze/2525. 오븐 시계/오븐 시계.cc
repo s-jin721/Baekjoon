8450d463f81fd2dd3c465912210ee3f40db86701
s-jin721 <lsj9134721@gmail.com>
2025-07-09 22:29:19 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -1,9 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-	int h, m;
-	int n;
-	scanf("%d %d", &h, &m);
-	scanf("%d", &n);
+	int h, m, n;
+	scanf("%d %d\n%d", &h, &m, &n);
 	printf("%d %d", (h + (n + m) / 60)%24, (n + m) % 60);
 }
