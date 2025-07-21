4740f60c17554577ae3cc72858f0f64af85683e0
s-jin721 <lsj9134721@gmail.com>
2025-07-21 10:36:51 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if ((n) % 4 == 0 || (n) % 4 ==2) {
+        printf("CY\n");
+    }
+    else {
+        printf("SK\n");
+    }
+
+    return 0;
+}
